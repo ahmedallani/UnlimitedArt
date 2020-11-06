4165734b521e5a5a42fa022c5de062286d645e96
ahmedallani <ahmed.allani@rbk.tn>
2020-11-06 09:10:26 +0100

    sign up

--- client/src/components/Freelancer/FreeLancerSignup.jsx
+++ client/src/components/Freelancer/FreeLancerSignup.jsx
@@ -1,60 +1,55 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import  { useState } from 'react';
-import axios from "axios"
-import {connect} from "react-redux"
-import { withRouter } from "react-router";
-
+import SelectField from "../Elements/FreelancerField.jsx";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
   },
   paper: {
     margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -62,50 +57,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    user:state.user
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    update: (value) => dispatch({
-      type: 'updatedata',
-      value
-    })
-  }
-}
-
-
-export default connect(mapStateToProps,mapDispatchToProps)(withRouter(function SignInSide(props) {
-  const [Email, setEmail] = useState("")
-  const [Password, setPassword] = useState("")
+export default function SignInSide() {
   const classes = useStyles();
-const Login = function (event) {
-  event.preventDefault();
-                axios({
-                    url: '/api/Freelancers/Login',
-                    method: 'post',
-                    data: {
-                        Email:Email,
-                        Password:Password
-                    }
-                }).then(data => {
-                    console.log(data.data)
-                    if(!data.data.Login){
-                        alert("Check Again")
-                    }else{
-                      props.history.push("/");
-                      props.change("freelancer")
-                      props.update(data.data.userData)
-                        }
-                    
-                }).catch(err => console.log(err))
-             
-        };
+  const [Field, setField] = useState("");
   return (
-    <Grid container component="main" className={classes.root}>
+    <Grid container component="main">
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -114,41 +70,96 @@ const Login = function (event) {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            FreeLancer Login
+            Sign in
           </Typography>
-          <form className={classes.form} onSubmit={Login} noValidate>
-            <TextField
-              onChange={e=>{
-                setEmail(e.target.value)
-              }}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-            onChange={e=>{
-              setPassword(e.target.value)
-            }}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
+          <form className={classes.form} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="fname"
+                  name="username"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="repass"
+                  label="Password Again"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="City"
+                  label="city"
+                  type="city"
+                  id="city"
+                  autoComplete="city"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="Adresse"
+                  label="adress"
+                  type="adress"
+                  id="adress"
+                  autoComplete="adress"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="Age"
+                  label="Age"
+                  type="number"
+                  id="Age"
+                />
+              </Grid>
+              {/* <Grid item xs={12} sm={8}>
+                <SelectField onChange={setField} />
+              </Grid> */}
+            </Grid>
             <Button
               type="submit"
               fullWidth
@@ -156,28 +167,18 @@ const Login = function (event) {
               color="primary"
               className={classes.submit}
             >
-              Sign In
+              Freelancer Sign Up
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
+            <G5rid container justify="flex-end">
               <Grid item>
                 <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                  Already have an account? Sign in
                 </Link>
               </Grid>
             </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
           </form>
         </div>
       </Grid>
     </Grid>
   );
-})
-);
-
+}
